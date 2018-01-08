43ed90a408a90fca189116b9f0b7401b21c815f9
Jesse Peplinski <peplinski.jesse@gmail.com>
2018-01-08 11:30:37 -0500

    toggleDrum function done. need to modularize

--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -1,35 +1,81 @@
 // Drum Arrays
-let kicks = new Array(16);
-let snares  = new Array (16);
-let hiHats = new Array(16);
-let rideCymbals = new Array(16);
-
-// Push false to arrays
-kicks.forEach(function(kick) {
-    kick.push(false);
-});
-
-snares.forEach(function(kick) {
-    snares.push(false);
-});
-
-hiHats.forEach(function(kick) {
-    hiHats.push(false);
-});
-
-rideCymbals.forEach(function(kick) {
-    rideCymbals.push(false);
-});
+let kicks = new Array(16).fill(false);
+let snares  = new Array (16).fill(false);
+let hiHats = new Array(16).fill(false);
+let rideCymbals = new Array(16).fill(false);
 
 // Takes two arguments: a string representing the array name ('kicks', 'snares', 'hiHats', or 'rideCymbals'), and an index number.
 // This function should flip the boolean value in the correct array at the specified index.
-function toggleDrum(drumArray, index) {
+function toggleDrum(drumArrayAsString, index) {
 
+    // console.log(drumArrayAsString + ' is the string we are trying to manipulate');
+    // console.log(index + ' is the index');
+
+    if (!drumArrayAsString || !index) {
+        // console.error('Arguments not specified.');
+    }
+
+    if(index > 16 || index < 0) {
+        // console.error('index out of range');
+    }
+
+    
+    switch(drumArrayAsString) {
+        case 'kicks':
+            if(kicks[index] === false) {
+                kicks[index] = true;
+            }
+            else if(kicks[index] === true) {
+                kicks[index] = false;
+            }
+            break;
+        case 'snares':
+            if(snares[index] === false) {
+                snares[index] = true;
+            }
+            else if(snares[index] === true) {
+                snares[index] = false;
+            }
+            break;  
+        case 'hiHats':
+            if(hiHats[index] === false) {
+                hiHats[index] = true;
+            }
+            else if(hiHats[index] === true) {
+                hiHats[index] = false;
+            }
+            break;   
+        case 'rideCymbals':
+            if(rideCymbals[index] === false) {
+                rideCymbals[index] = true;
+            }
+            else if(rideCymbals[index] === true) {
+                rideCymbals[index] = false;
+            }
+            break; 
+        default:
+            console.error('Kicks, snares, hiHats, or rideCymbals not specified');    
+    }
 }
 
 // takes an array name string and sets all values in the correct array to false.
 function clear(drumArray) {
-
+    switch(drumArray) {
+        case 'kicks':
+            kicks.fill(false);
+            break;
+        case 'snares':
+            snares.fill(false);
+            break;  
+        case 'hiHats':
+            hiHats.fill(false);
+            break;   
+        case 'rideCymbals':
+            rideCymbals.fill(false);
+            break; 
+        default:
+            // console.log('Kicks, snares, hiHats, or rideCymbals not specified');    
+    }
 }
 
 // takes an array name string and flips the boolean value of all elements in the correct array.
@@ -37,3 +83,8 @@ function invert(drumArray) {
 
 }
 
+// console.log(kicks);
+
+// toggleDrum('kicks', 6);
+
+// console.log(kicks);
