0397b7253463461db16a0877b48eaae2f3f84bb7
Jesse Peplinski <peplinski.jesse@gmail.com>
2018-01-16 21:21:21 -0500

    first capstone push

--- capstone-project-1-x-press-publishing/api/api.js
+++ capstone-project-1-x-press-publishing/api/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const apiRouter = express.Router();
-
-const artistsRouter = require('./artists');
+const artistsRouter = require('./artists.js');
+const seriesRouter = require('./series.js');
 
 apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/series', seriesRouter);
 
 module.exports = apiRouter;
